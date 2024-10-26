9ce376db9d5b8811158c9fc1a6ba7b78a5a4b181
Thomas <74470527+Batacek@users.noreply.github.com>
2024-10-26 15:58:17 +0200

    Updating main.py and adding libraries
    adding lcd_api and pico_i2c_lcd libraries

--- main.py
+++ main.py
@@ -1,7 +1,17 @@
 import machine
+from machine import I2C, Pin
 import onewire
 import ds18x20
 import time
+from lcd_api import LcdApi
+from pico_i2c_lcd import I2cLcd
+
+I2C_ADDR     = 39
+I2C_NUM_ROWS = 4
+I2C_NUM_COLS = 20
+
+i2c = I2C(0, sda=machine.Pin(0), scl=machine.Pin(1), freq=400000)
+lcd = I2cLcd(i2c, I2C_ADDR, I2C_NUM_ROWS, I2C_NUM_COLS)
 
 ts_pin = machine.Pin(28)
 builtin_ts_pin = machine.ADC(4)
@@ -14,13 +24,25 @@ roms = s.scan()
 while True:
     s.convert_temp()
     time.sleep_ms(750)
+    
+    lcd.clear()
+    lcd.move_to(0,0)
 
     for rom in roms:
+        
+        T1 = "Temp1:" + str(s.read_temp(rom)) + " C"
+        T2 = "Temp2:" + str(-1 * (27 - (bts - 0.706) / 0.001721)) + " C"
+        T3 = "Temp2:" + str(27 - (bts - 0.706) / 0.001721) + " C"
+        
         print("Temperature (Sensor):	", s.read_temp(rom), "°C")
+        lcd.putstr(str(T1))
     if ((27 - (bts - 0.706) / 0.001721) < 0):
         print("Temperature (Built-in):	", -1 * (27 - (bts - 0.706) / 0.001721), "°C")
+        lcd.move_to(0,1)
+        lcd.putstr(str(T2))
     elif ((27 - (bts - 0.706) / 0.001721) >= 0):
         print("Temperature (Built-in):	", (27 - (bts - 0.706) / 0.001721), "°C")
+        lcd.move_to(0,1)
+        lcd.putstr(str(T3))
 
     time.sleep_ms(250)
-
